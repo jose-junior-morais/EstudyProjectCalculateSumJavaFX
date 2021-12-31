d8b74c3374b4fb49fb841e0f78e5c8755034801f
Jose Junior <juniormilian6@gmail.com>
2021-12-31 08:52:26 -0300

    method and variable class viewController

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,18 +1,35 @@
 package gui;
 
+import java.util.Locale;
+
 import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldListCell;
 
 public class ViewController {
+    @FXML
+	private TextField txtNumber1;
+	
+    @FXML
+    private TextField txtNumber2;
+    
+    @FXML
+    private Label lbResultSum;
     
 	@FXML
-	private Button btTest;
+	private Button sum;
 	
 	@FXML
-	public void onBtTestAction() {
-		Alerts.showAlert("Alert title", "Alert header", "Hello",AlertType.CONFIRMATION);
+	public void onBtnAction() {
+		Locale.setDefault(Locale.US);
+		Double number1=Double.parseDouble(txtNumber1.getText());
+		Double number2=Double.parseDouble(txtNumber2.getText());
+		Double sum=number1+number2;
+		lbResultSum.setText(String.format("%.2f", sum));
 	}
 	
 }
